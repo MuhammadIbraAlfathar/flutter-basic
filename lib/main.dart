455f41c2d0d855ea7e9571cac404e4eb0975c013
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-31 12:54:37 +0700

    belajar membuat column di flutter

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,46 @@ class MyApp extends StatelessWidget {
         // useMaterial3: true,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Jago Flutter"),
-        ),
-        body: const ChangeTimeWidget(),
+          appBar: AppBar(
+            title: const Text("Jago Flutter"),
+          ),
+          body: const ColumnExample()),
+    );
+  }
+}
+
+class ColumnExample extends StatefulWidget {
+  const ColumnExample({super.key});
+
+  @override
+  State<ColumnExample> createState() => _ColumnExample();
+}
+
+class _ColumnExample extends State<ColumnExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Stylish Chair',
+            style: TextStyle(
+                color: Colors.black, fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Rp.350.000',
+            style: TextStyle(
+                fontSize: 20,
+                color: Color(0xFF9A9390),
+                fontWeight: FontWeight.w400,
+                letterSpacing: 1),
+          )
+        ],
       ),
     );
   }
@@ -44,16 +80,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -91,23 +117,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
