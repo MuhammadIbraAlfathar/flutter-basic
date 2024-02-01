5d297c5ed2f55abc16eb708fa252d4fb37766f51
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-02-01 14:02:21 +0700

    [basic-form] membuat widget dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widget_lesson/basic_layout/stack_widget.dart';
-import 'package:flutter_application_1/widget_lesson/basic_layout/wrap_widget.dart';
+import 'package:flutter_application_1/widget_lesson/basic_form/dialog_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,7 +33,7 @@ class MyApp extends StatelessWidget {
         // primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: WrapWidget(),
+      home: const DialogWidget(),
     );
   }
 }
---
+++ lib/widget_lesson/basic_form/dialog_widget.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class DialogWidget extends StatefulWidget {
+  const DialogWidget({super.key});
+
+  @override
+  State<DialogWidget> createState() => _DialogWidgetState();
+}
+
+class _DialogWidgetState extends State<DialogWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter - Dialog & Bottomsheet"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Info"),
+                      content: const SingleChildScrollView(
+                        child: ListBody(
+                          children: [Text("Your order was placed!")],
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("OK"))
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text("Open Dialog"),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await showModalBottomSheet<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text("Your order was placed!"),
+                              const SizedBox(
+                                height: 20.0,
+                              ),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("OK"))
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: const Text("Open BottomSheet")),
+            ElevatedButton(
+              onPressed: () async {
+                // ignore: unused_local_variable
+                bool confirm = false;
+                await showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Wrap(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                const Text(
+                                  "Confirm",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                const Text(
+                                    "Are you sure want to delete this item?"),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          confirm = true;
+                                          Navigator.pop(context);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.blueGrey),
+                                        child: const Text("YES!")),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+
+                if (confirm) {
+                  print("Confirmed");
+                }
+              },
+              child: const Text('Open Bottomsheet confirmation'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Your request is successful"),
+                    ),
+                  );
+                },
+                child: const Text("Open Snackbar"))
+          ],
+        ),
+      ),
+    );
+  }
+}
